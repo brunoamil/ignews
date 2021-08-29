75227c93608a0d3565626f569a5ec1a11d396b12
Bruno <bruno-amil@hotmail.com>
2021-08-29 12:18:53 -0300

    criando o componente subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../compononents/SubscribeButton";
 import styles from "./home.module.scss";
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
             Get access to all the pulications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Garota programando" />
       </main>
