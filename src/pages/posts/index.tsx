d86ca006a5c7348db84699fa135c0a6f5da965ca
Bruno <bruno-amil@hotmail.com>
2021-09-04 22:21:11 -0300

    criando a pagina de post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import Prismic from "@prismicio/client";
 import { RichText } from "prismic-dom";
 import { getPrismicCLient } from "../../services/prismic";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 interface Post {
   slug: string;
@@ -24,11 +25,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#sdsds">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
